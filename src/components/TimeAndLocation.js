f626ca496c0c99113e902f98a0ffc8fe0bd29553
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-09-27 00:47:05 +0500

    Made Time  and Location Component and Completed it

---
+++ src/components/TimeAndLocation.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function TimeAndLocation() {
+  return (
+    <>
+    <div className="flex justify-center my-6 items-center">
+      <p className="text-white text-xl font-extralight">
+        Tuesday, 31 May 2022 Local time: 12:46 PM
+      </p>
+      
+    </div>
+
+    <div className="flex items-center justify-center my-3">
+    <p className="text-white text-3xl"> Karachi, PK</p>
+    </div>
+
+    </>
+  );
+}
